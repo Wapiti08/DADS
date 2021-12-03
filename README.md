ffb14cbcc32165f607af1cbb44d0037538956d82
tzrzhuoran@gmail.com <tzrzhuoran@gmail.com>
2021-12-03 16:05:50 +0000

    add some comment for structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Small_Distributed_System
 the practice with Go to build distributed systems
+
+## Features:
+- one master node, several slave nodes
+- communicate through TCP protocols
+- messages are json packets
+- each node has its unique ID
+- first node is default the master cluster. If it dies, re-elect a master
+
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+import (
+    "flag"
+    "fmt"
+    "net"
+    "strings"
+    "strconv"
+    "time"
+    "math/rand"
+    "encoding/json"
+)
+// information/metadata about node
+type NodeInfo struct {
+}
+
+// standard format for a Rq/Rp for adding node to cluster
+
+type AddToClusterMessage struct {
+}
+
+// format print for node info
+
+// format print for Rq/Rp info
+
+// entrypoint for system
+
+/* format the json packet to send requests */
+
+// unexported function
+func connectToCluster() (){
+	
+}
+
+func listenOnPort() {
+
+}
