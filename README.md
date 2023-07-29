9f040af89bf1231323334870ac8d104bae5d0ecd
谭卓然 <newt.tan@dbappsecurity.com.cn>
2023-07-29 21:21:07 +0100

    build first prototype

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ the practice with Go to build distributed systems
 - each node has its unique ID
 - first node is default the master cluster. If it dies, re-elect a master
 
+## References
+
+*https://dev.to/tikazyq/golang-in-action-how-to-implement-a-simple-distributed-system-2n0n*
+*https://didil.medium.com/building-a-simple-distributed-system-with-go-consul-39b08ffc5d2c*
+*https://confusedcoders.com/general-programming/go-lang/create-a-basic-distributed-system-in-go-lang-part-1*
+*https://confusedcoders.com/general-programming/go-lang/create-a-basic-distributed-system-in-go-lang-part-2-http-server-json-requestresponse*
--- main.go
+++ main.go
@@ -1,21 +1,24 @@
 package main
+
 import (
     "flag"
-    "fmt"
     "net"
-    "strings"
-    "strconv"
-    "time"
-    "math/rand"
     "encoding/json"
+    "math/rand"
+    "time"
 )
+
+
 // information/metadata about node
 type NodeInfo struct {
+    NodeId int  'json:"nodeid"'
+    NodeIp string   'json:"nodeip"'
+    Port string 'json:"port"'
 }
 
 // standard format for a Rq/Rp for adding node to cluster
-
 type AddToClusterMessage struct {
+
 }
 
 // format print for node info
@@ -23,14 +26,28 @@ type AddToClusterMessage struct {
 // format print for Rq/Rp info
 
 // entrypoint for system
+func main() {
+    clusterip := flag.String("clusterip", "127.0.0.1","if not connected to another cluster, set itself as master")
+    port := flag.Int("port"."8001", "connect to this port")
+    masterNodeError := flag.Bool("masterNodeError")
+    flat.parse()
+    // generate the unique id for cluster with rand and time
+    rand.seed(time.Now().UTC().UnixNano())
+    randid := rand.Intn(999999)
+    // assign values to node
+    ip, _ := net.InterfaceAddrs()
+    source := NodeInfo{Nodeid: randid, NodeIp:ip, Port: *port}
 
+}
 /* format the json packet to send requests */
 
 // unexported function
-func connectToCluster() (){
-	
+func connectToCluster() {
+    // protocol type, address, timout duration
+    conn, err := net.DailTimeout()
 }
 
 func listenOnPort() {
-
+    // protocol type, address
+    In, err := net.Listen("")
 }
